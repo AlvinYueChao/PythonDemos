f9aca38913ebff0dcfa6cc07073780d6aa715e7f
YueChao <yue327305210@163.com>
2021-08-07 10:38:29 +0800

    fixed ut error for format_map demo

--- study/stringMethods.py
+++ study/stringMethods.py
@@ -4,7 +4,7 @@ import unittest
 class MyTestCase(unittest.TestCase):
     def test_format(self):
         string = '{name} is from {country}'
-        result = string.format_map(name='alvin', country='China')
+        result = string.format_map(dict(name='alvin', country='China'))
         self.assertEqual(result, 'alvin is from China')
 
 if __name__ == '__main__':
