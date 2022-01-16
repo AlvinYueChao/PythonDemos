f525b18f9a808a2c6934bd01b5234aa78f5a7ea5
YueChao <yue327305210@163.com>
2022-01-16 20:45:19 +0800

    2022 0116 2045

--- org/example/alvin/test/test.py
+++ org/example/alvin/test/test.py
@@ -9,6 +9,7 @@ def test_parse_qs():
     print('Green:   ', my_values.get('green'))
     print('Opacity: ', my_values.get('opacity'))
 
+
 def test_zip():
     years = {2019, 2020, 2021, 2022}
     months = {'Jan', 'Feb', 'Mar', 'Apr', 'May'}
@@ -18,6 +19,46 @@ def test_zip():
         print(f'Today is: {year}-{month}-{day}')
 
 
+def test_sort():
+    class Tool:
+        def __init__(self, name, weight):
+            self.name = name
+            self.weight = weight
+
+        def __repr__(self):
+            return f'Tool({self.name!r}, {self.weight})'
+
+    power_tools = [Tool('drill', 4), Tool('circular saw', 5), Tool('jackhammer', 40), Tool('sander', 4)]
+
+    # 先按 weight 降序排序，再按 name 升序排序
+    print('order by weight desc firstly, order by name asc finally')
+    power_tools.sort(key=lambda x: x.name)
+    print(power_tools)
+    power_tools.sort(key=lambda x: x.weight, reverse=True)
+    print(power_tools)
+
+    # 先按 name 升序排序，再按 weight 降序排序
+    print('order by name asc firstly, order by weight desc finally')
+    power_tools.sort(key=lambda x: x.weight, reverse=True)
+    print(power_tools)
+    power_tools.sort(key=lambda x: x.name)
+    print(power_tools)
+
+    # 先按 weight 升序排序，再按 name 降序排序
+    print('order by weight asc firstly, order by name desc finally')
+    power_tools.sort(key=lambda x: x.name, reverse=True)
+    print(power_tools)
+    power_tools.sort(key=lambda x: x.weight)
+    print(power_tools)
+
+    # 先按 name 降序排序，再按 weight 升序排序
+    print('order by name desc firstly, order by weight asc finally')
+    power_tools.sort(key=lambda x: x.weight)
+    print(power_tools)
+    power_tools.sort(key=lambda x: x.name, reverse=True)
+    print(power_tools)
+
 if __name__ == '__main__':
     # test_parse_qs()
-    test_zip()
+    # test_zip()
+    test_sort()
